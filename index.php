373602769a79ebe3458ad5d41c3da40212a12167
Tristan Amiotte-Suchet <47942253+Blanc-Poil@users.noreply.github.com>
2022-12-01 19:30:24 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style/index.css" />
+        <title>SIDASH</title>
+    </head>
+    <body>
+        salut
+    </body>
+</html>
