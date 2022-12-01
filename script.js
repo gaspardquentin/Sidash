19cc32b31b1926e2a0761e3cf4d52311323a5b3c
Gaspard Quentin <gaspard.quentin1905@gmail.com>
2022-12-01 17:53:34 +0100

    CANVAS

---
+++ script.js
@@ -0,0 +1,8 @@
+const gameCanvas = document.getElementById("gameCanvas");
+
+const ctx = gameCanvas.getContext("2d");
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+ctx.fillStyle = "rgba(0,0,0,1)";
+ctx.rect(0, 0, gameCanvas.width, gameCanvas.height);
+ctx.fill();
