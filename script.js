547a33912a5f4496c485aa7fbcdd5b42e58069df
Gaspard Quentin <gaspard.quentin1905@gmail.com>
2022-12-01 18:34:59 +0100

    commit

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
-const gameCanvas = document.getElementById("gameCanvas");
+window.addEventListener ('load', function(){
+    const canvas = document. getElementById('');
+    const ctx = canvas.getContext ('2d') ;
+    canvas.width = 800;
+    canvas. height = 720;
+    
+    class InputHandler {
+        constructor (){
+            this.keys = [];
+            window.addEventListener ('keydown' , e => {
+                if (( e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight')
+                    && this.keys.indexof(e.key) === -1){
+                this.keys.push(e.key);
+                }
+            });
+            window.addEventListener('keyup', e => {
+            if (
+                e.key === 'ArrowDown' ||
+                e.key === 'ArrowUp' ||
+                e.key === 'ArrowLeft' ||
+                e.key ==='ArrowRight'){
+            this.keys.splice(this.keys.indexOf(e.key), 1);
+            }
+        });
+        }
+    }
+    class Player {
+        constructor (gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 200;
+            this.height = 200;
+            this.x = 10;
+            this.y = 10;
+        }
+        draw (context){
+            context.fillStyle = 'white';
+            context.fillRect(this.x, this.y, this.width, this .height);
+        }
+    }
+    class Background {
+    }
+    class Enemy {
+    }
+    function handleEnemies (){
+    }
+    function displayStatusText (){
+    }
+    const input = new InputHandler();
+    const player = new Player();
+    player.draw(ctx);
 
-const ctx = gameCanvas.getContext("2d");
-ctx.canvas.width  = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
-ctx.fillStyle = "rgba(0,0,0,1)";
-ctx.rect(0, 0, gameCanvas.width, gameCanvas.height);
-ctx.fill();
+    function animate(){
+    }
+});
